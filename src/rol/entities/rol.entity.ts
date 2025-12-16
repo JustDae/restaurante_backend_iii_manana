54cd36cf0231448513cdb75b7840d3b4f9fcd617
natcore-tech <asesoriaelectr@gmail.com>
2025-12-16 12:31:18 -0500

    update users and rol

--- src/rol/entities/rol.entity.ts
+++ src/rol/entities/rol.entity.ts
@@ -1,1 +1,20 @@
-export class Rol {}
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { User } from '../../users/user.entity';
+
+@Entity('rol')
+export class Rol {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  nombre: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  descripcion: string;
+
+  @Column({ type: 'boolean', default: true })
+  activo: boolean;
+
+  @OneToMany(() => User, (user) => user.rol)
+  users: User[];
+}
