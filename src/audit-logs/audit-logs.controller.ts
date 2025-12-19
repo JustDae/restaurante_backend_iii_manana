0f61f7ca1b76eaf128bdc36abd1612042c911b76
hyxuu <hyxuu05@gmail.com>
2025-12-18 22:28:34 -0500

    update: set audit-logs as read-only

--- src/audit-logs/audit-logs.controller.ts
+++ src/audit-logs/audit-logs.controller.ts
@@ -1,15 +1,9 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Body } from '@nestjs/common';
 import { AuditLogsService } from './audit-logs.service';
-import { CreateAuditLogDto } from './dto/create-audit-log.dto';
 
 @Controller('audit-logs')
 export class AuditLogsController {
   constructor(private readonly auditLogsService: AuditLogsService) {}
-
-  @Post()
-  create(@Body() dto: CreateAuditLogDto) {
-    return this.auditLogsService.create(dto);
-  }
 
   @Get()
   findAll() {
