cc8d2ff173e8a64a037e989d24098706b2263c41
JulanT0811 <jtlengua.9b@gmail.com>
2025-12-19 22:43:04 -0500

    modified dto resenas

--- src/resenas/dto/create-contenido.dto.ts
+++ src/resenas/dto/create-contenido.dto.ts
@@ -1,20 +1,11 @@
-import { IsString, IsOptional, IsInt, Min, Max } from 'class-validator';
+import { IsInt, Min, Max, IsString } from 'class-validator';
 
 export class CreateContenidoDto {
   @IsInt()
   @Min(1)
   @Max(5)
-  calificacion: number; // 1 a 5 estrellas
+  rating: number; 
 
-  @IsOptional()
   @IsString()
-  comentario?: string;
-
-  @IsOptional()
-  @IsString()
-  categoria?: string; 
-
-  @IsOptional()
-  @IsString()
-  pedidoId?: string; 
+  comentario: string; 
 }
