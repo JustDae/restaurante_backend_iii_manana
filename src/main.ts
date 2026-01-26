a7556a8ed33cf299a2fa16f859064b0978d27977
natcore-tech <asesoriaelectr@gmail.com>
2026-01-25 19:14:07 -0500

    add url front

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,7 @@ async function bootstrap() {
     origin: [
       'http://localhost:5173',
       'http://localhost:3000',
+      'https://restaurante-app.nael.live',
       'https://api-restaurante.nael.live',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
